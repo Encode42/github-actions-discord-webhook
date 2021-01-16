6c6d953fa4b12f8cb2502543097cefaa0cc547b8
Encode42 <me@encode42.dev>
2021-01-16 13:14:22 -0500

    Cleanup and organization

--- README.md
+++ README.md
@@ -1,24 +1,21 @@
 # Discord Webhook for Github Actions
-**Modified to have the same style as the default GitHub webhook.**
-
-Note: This is mainly for my own personal use! There will be no support provided.
+**Modified to have the same style as the default GitHub webhook.**  
+This is mainly for my own personal use! There will be little to no support provided.
 
 ## Setup
-1. In Github secrets, add a `WEBHOOK_URL` variable with the Discord web hook URL
-1. In your Github actions yml file, add this to reference the variable you just created:
-    - To see a real example, visit [here](https://github.com/Encode42/MatrixChecks-Plugin/blob/9b53a421c036b7f6197f6a5052a425a96eaa343d/.github/workflows/run.yml#L48).
+1. In Github secrets, add a `WEBHOOK_URL` variable with the Discord web hook URL.
+2. Inside your workflow file, add this snippet after the build-critical task:
+
     ```yaml
         - uses: actions/setup-ruby@v1
         - name: Send Webhook Notification
           if: always()
-          env:
-            JOB_STATUS: ${{ job.status }}
-            WEBHOOK_URL: ${{ secrets.WEBHOOK_URL }}
-            HOOK_OS_NAME: ${{ runner.os }}
-            WORKFLOW_NAME: ${{ github.workflow }}
           run: |
             git clone https://github.com/Encode42/discord-workflows-webhook.git webhook
-            bash webhook/send.sh $JOB_STATUS $WEBHOOK_URL
+            bash webhook/send.sh ${{ job.status }} ${{ secrets.WEBHOOK_URL }}
           shell: bash
     ```
-1. Enjoy!
+
+    <sub>To see a working example, look at [this workflow file](https://github.com/Encode42/MatrixChecks-Plugin/blob/main/.github/workflows/run.yml).</sub>  
+    <sub>Note: Excluding the Ruby setup action will automatically disable the PR title getter.</sub>
+3. That's all! Notifications will be sent on every workflow run.
--- send.sh
+++ send.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 # This original source of this code: https://github.com/DiscordHooks/travis-ci-discord-webhook
-# The same functionality from TravisCI is needed for Github Actions
-#
-# For info on the GITHUB prefixed variables, visit:
-# https://help.github.com/en/articles/virtual-environments-for-github-actions#environment-variables
 
-# More info: https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
+# Get the build status
 case ${1,,} in
   "success" )
     EMBED_COLOR=38912
@@ -23,74 +19,69 @@ case ${1,,} in
     ;;
 esac
 
+# Check for the webhook argument
 shift
+if [ $# -lt 1 ]; then echo -e "The second argument of this script must be the WEBHOOK_URL environment variable. https://github.com/Encode42/discord-workflows-webhook" && exit; fi
 
-if [ $# -lt 1 ]; then
-  echo -e "WARNING!!\nYou need to pass the WEBHOOK_URL environment variable as the second argument to this script.\nFor details & guide, visit: https://github.com/DiscordHooks/github-actions-discord-webhook" && exit
-fi
-
+# Author details
 AUTHOR_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%aN")"
 AUTHOR_URL="https://github.com/$AUTHOR_NAME"
+AUTHOR_AVATAR="https://github.com/$AUTHOR_NAME.png"
+
+# Commit details
 COMMITTER_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%cN")"
 COMMIT_SUBJECT="$(git log -1 "$GITHUB_SHA" --pretty="%s")"
-COMMIT_MESSAGE="$(git log -1 "$GITHUB_SHA" --pretty="%b")" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g'
 COMMIT_URL="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
 
-AVATAR="https://github.com/$AUTHOR_NAME.png"
-
-# If, for example, $GITHUB_REF = refs/heads/feature/example-branch
-# Then this sed command returns: feature/example-branch
-BRANCH_NAME="$(echo $GITHUB_REF | sed 's/^[^/]*\/[^/]*\///g')"
+# Branch details
 REPO_URL="https://github.com/$GITHUB_REPOSITORY"
-BRANCH_OR_PR="Branch"
+REPO_NAME="$(echo "$GITHUB_REPOSITORY" | sed 's/^[^\/]*\///')"
+BRANCH_NAME="$(echo "$GITHUB_REF" | sed 's/^[^/]*\/[^/]*\///g')"
 BRANCH_OR_PR_URL="$REPO_URL/tree/$BRANCH_NAME"
-ACTION_URL="$COMMIT_URL/checks"
-COMMIT_OR_PR_URL=$COMMIT_URL
-if [ "$AUTHOR_NAME" == "$COMMITTER_NAME" ]; then
-  CREDITS="$AUTHOR_NAME authored & committed"
-else
-  CREDITS="$AUTHOR_NAME authored & $COMMITTER_NAME committed"
-fi
+ACTION_URL="$REPO_URL/actions/runs/$GITHUB_RUN_ID"
 
+# PR details
 if [ "$GITHUB_EVENT_NAME" == "pull_request" ]; then
-	BRANCH_OR_PR="Pull Request"
-	
-	PR_NUM=$(sed 's/\/.*//g' <<< $BRANCH_NAME)
+	PR_NUM=$(sed 's/\/.*//g' <<< "$BRANCH_NAME")
 	BRANCH_OR_PR_URL="$REPO_URL/pull/$PR_NUM"
 	BRANCH_NAME="#${PR_NUM}"
-	
-	# Call to GitHub API to get PR title
 	PULL_REQUEST_ENDPOINT="https://api.github.com/repos/$GITHUB_REPOSITORY/pulls/$PR_NUM"
 	
-	WORK_DIR=$(dirname ${BASH_SOURCE[0]})
-	PULL_REQUEST_TITLE=$(ruby $WORK_DIR/get_pull_request_title.rb $PULL_REQUEST_ENDPOINT)
+	# Check if the Ruby command is set up
+	# If not, PR title = PR number
+	if command -v ruby &> /dev/null; then
+		WORK_DIR=$(dirname "${BASH_SOURCE[0]}")
+		PULL_REQUEST_TITLE=$(ruby "$WORK_DIR"/get_pull_request_title.rb "$PULL_REQUEST_ENDPOINT")
+	else
+		echo -e "Ruby command not found, PR title getting skipped..."
+		PULL_REQUEST_TITLE=$PR_NUM
+	fi
 	
 	COMMIT_SUBJECT=$PULL_REQUEST_TITLE
-	COMMIT_MESSAGE="Pull Request #$PR_NUM"
 	ACTION_URL="$BRANCH_OR_PR_URL/checks"
-	COMMIT_OR_PR_URL=$BRANCH_OR_PR_URL
 fi
 
-TIMESTAMP=$(date -u +%FT%TZ)
+# Compile the webhook data
 WEBHOOK_DATA='{
   "avatar_url": "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
-  "embeds": [ {
+  "embeds": [{
     "color": '$EMBED_COLOR',
     "author": {
-      "name": "'"$COMMITTER_NAME"'",
-      "icon_url": "'$AVATAR'",
+      "name": "'"$AUTHOR_NAME"'",
+      "icon_url": "'$AUTHOR_AVATAR'",
       "url": "'$AUTHOR_URL'"
     },
-    "title": "['"$(echo $GITHUB_REPOSITORY | sed 's/^[^\/]*\///')"':'"$BRANCH_NAME"'] Build '"$STATUS_MESSAGE"'",
-    "url": "'"$REPO_URL"'/actions/runs/'"$GITHUB_RUN_ID"'",
+    "title": "['"$REPO_NAME"':'"$BRANCH_NAME"'] Build '"$STATUS_MESSAGE"'",
+    "url": "'"$ACTION_URL"'",
     "description": "'"[\`${GITHUB_SHA:0:7}\`](${COMMIT_URL})"' '"$COMMIT_SUBJECT"' - '"$COMMITTER_NAME"'"
-  } ]
+  }]
 }'
 
 for ARG in "$@"; do
-  echo -e "[Webhook]: Sending webhook to Discord...\\n";
-  echo -e "$WEBHOOK_DATA"
+  echo -e "[Webhook]: Compiled JSON webhook data: $WEBHOOK_DATA\\n"
 
+  # Send the webhook
+  echo -e "[Webhook]: Sending webhook to Discord...\\n";
   (curl --fail --progress-bar -A "GitHub-Actions-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "${WEBHOOK_DATA//	/ }" "$ARG" \
   && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
 done
