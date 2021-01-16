43a45801cc7f06b334a13b1439742097f18295d0
Encode42 <me@encode42.dev>
2021-01-16 11:49:59 -0500

    Default webhook styling

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
 # Discord Webhook for Github Actions
+**Modified to have the same style as the default GitHub webhook.**
+
+Note: This is mainly for my own personal use! There will be no support provided.
+
 ## Setup
 1. In Github secrets, add a `WEBHOOK_URL` variable with the Discord web hook URL
 1. In your Github actions yml file, add this to reference the variable you just created:
-    - To see a real example, visit [here](https://github.com/unthreaded/git-hooks/blob/92ea6bde348431fbe25d05c33398c969eec5d3ee/.github/workflows/build.yml#L48).
+    - To see a real example, visit [here](https://github.com/Encode42/MatrixChecks-Plugin/blob/9b53a421c036b7f6197f6a5052a425a96eaa343d/.github/workflows/run.yml#L48).
     ```yaml
         - uses: actions/setup-ruby@v1
         - name: Send Webhook Notification
@@ -13,7 +17,7 @@
             HOOK_OS_NAME: ${{ runner.os }}
             WORKFLOW_NAME: ${{ github.workflow }}
           run: |
-            git clone https://github.com/DiscordHooks/github-actions-discord-webhook.git webhook
+            git clone https://github.com/Encode42/discord-workflows-webhook.git webhook
             bash webhook/send.sh $JOB_STATUS $WEBHOOK_URL
           shell: bash
     ```
--- send.sh
+++ send.sh
@@ -5,23 +5,21 @@
 # For info on the GITHUB prefixed variables, visit:
 # https://help.github.com/en/articles/virtual-environments-for-github-actions#environment-variables
 
-AVATAR="https://github.com/actions.png"
-
 # More info: https://www.gnu.org/software/bash/manual/bash.html#Shell-Parameter-Expansion
 case ${1,,} in
   "success" )
-    EMBED_COLOR=3066993
-    STATUS_MESSAGE="Passed"
+    EMBED_COLOR=38912
+    STATUS_MESSAGE="passed"
     ;;
 
   "failure" )
-    EMBED_COLOR=15158332
-    STATUS_MESSAGE="Failed"
+    EMBED_COLOR=16525609
+    STATUS_MESSAGE="failed"
     ;;
 
   * )
-    STATUS_MESSAGE="Status Unknown"
-    EMBED_COLOR=0
+    EMBED_COLOR=2105893
+    STATUS_MESSAGE="unknown"
     ;;
 esac
 
@@ -32,10 +30,13 @@ if [ $# -lt 1 ]; then
 fi
 
 AUTHOR_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%aN")"
+AUTHOR_URL="https://github.com/$AUTHOR_NAME"
 COMMITTER_NAME="$(git log -1 "$GITHUB_SHA" --pretty="%cN")"
 COMMIT_SUBJECT="$(git log -1 "$GITHUB_SHA" --pretty="%s")"
 COMMIT_MESSAGE="$(git log -1 "$GITHUB_SHA" --pretty="%b")" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g'
 COMMIT_URL="https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+
+AVATAR="https://github.com/$AUTHOR_NAME.png"
 
 # If, for example, $GITHUB_REF = refs/heads/feature/example-branch
 # Then this sed command returns: feature/example-branch
@@ -72,36 +73,23 @@ fi
 
 TIMESTAMP=$(date -u +%FT%TZ)
 WEBHOOK_DATA='{
-  "username": "",
-  "avatar_url": "'$AVATAR'",
+  "avatar_url": "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
   "embeds": [ {
     "color": '$EMBED_COLOR',
     "author": {
-      "name": "'"$STATUS_MESSAGE"': '"$WORKFLOW_NAME"' ('"${HOOK_OS_NAME}"') - '"$GITHUB_REPOSITORY"'",
-      "url": "'$ACTION_URL'",
-      "icon_url": "'$AVATAR'"
+      "name": "'"$COMMITTER_NAME"'",
+      "icon_url": "'$AVATAR'",
+      "url": "'$AUTHOR_URL'"
     },
-    "title": "'"$COMMIT_SUBJECT"'",
-    "url": "'"$COMMIT_OR_PR_URL"'",
-    "description": "'"${COMMIT_MESSAGE//$'\n'/ }"\\n\\n"$CREDITS"'",
-    "fields": [
-      {
-        "name": "Commit",
-        "value": "'"[\`${GITHUB_SHA:0:7}\`](${COMMIT_URL})"'",
-        "inline": true
-      },
-      {
-        "name": "'"$BRANCH_OR_PR"'",
-        "value": "'"[\`${BRANCH_NAME}\`](${BRANCH_OR_PR_URL})"'",
-        "inline": true
-      }
-    ],
-    "timestamp": "'"$TIMESTAMP"'"
+    "title": "['"$(echo $GITHUB_REPOSITORY | sed 's/^[^\/]*\///')"':'"$BRANCH_NAME"'] Build '"$STATUS_MESSAGE"'",
+    "url": "'"$REPO_URL"'/actions/runs/'"$GITHUB_RUN_ID"'",
+    "description": "'"[\`${GITHUB_SHA:0:7}\`](${COMMIT_URL})"' '"$COMMIT_SUBJECT"' - '"$COMMITTER_NAME"'"
   } ]
 }'
 
 for ARG in "$@"; do
   echo -e "[Webhook]: Sending webhook to Discord...\\n";
+  echo -e "$WEBHOOK_DATA"
 
   (curl --fail --progress-bar -A "GitHub-Actions-Webhook" -H Content-Type:application/json -H X-Author:k3rn31p4nic#8383 -d "${WEBHOOK_DATA//	/ }" "$ARG" \
   && echo -e "\\n[Webhook]: Successfully sent the webhook.") || echo -e "\\n[Webhook]: Unable to send webhook."
